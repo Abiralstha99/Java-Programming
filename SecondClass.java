c868807f3058176a3aa32203e30aa38dfddd9312
Abiral Stha <abiralstha333@gmail.com>
2024-03-05 23:20:32 +0545

    Uploaded Files for day 2,3 and 4

--- SecondClass.java
+++ SecondClass.java
@@ -15,7 +15,7 @@ class SecondClass {
             Scanner sc = new Scanner (System.in);
             System.out.println("Enter first number");
             int a = sc.nextInt();
-            System.out.println("Enter first number");
+            System.out.println("Enter second number");
             int b = sc.nextInt();
             int sum = a+b;
             System.out.println("The sum is " + sum);
