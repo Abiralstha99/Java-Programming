64aecd87ab45bdfc130043dbe159d7e90f33df71
Abiral Stha <abiralstha333@gmail.com>
2024-03-07 18:11:34 +0545

    Uploaded files for day 4

--- Thirdclass.java
+++ Thirdclass.java
@@ -1,7 +1,27 @@
-<<<<<<< HEAD
 import java.util.*;
 public class Thirdclass {
     public static void main (String [] args){
+
+
+    //To check whether a or b is greater
+   /*  Scanner sc = new Scanner (System.in);
+    System.out.println("Enter 1st number");
+    int a = sc.nextInt();
+    System.out.println("Enter 2nd number");
+    int b = sc.nextInt();
+    if (a>b){
+        System.out.println(a + " is the greater number");
+    }
+    else if (b>a){
+        System.out.println(b + " is the greater number ");
+    }
+    else {
+        System.out.println( a + " and " +  b  + " are equal");
+    }
+    sc.close();
+    }
+}
+*/
 
 
     //To check whether a or b is greater
@@ -22,28 +42,3 @@ public class Thirdclass {
     sc.close();
     }
 }
-=======
-import java.util.*;
-public class Thirdclass {
-    public static void main (String [] args){
-
-
-    //To check whether a or b is greater
-    Scanner sc = new Scanner (System.in);
-    System.out.println("Enter 1st number");
-    int a = sc.nextInt();
-    System.out.println("Enter 2nd number");
-    int b = sc.nextInt();
-    if (a>b){
-        System.out.println(a + " is the greater number");
-    }
-    else if (b>a){
-        System.out.println(b + " is the greater number ");
-    }
-    else {
-        System.out.println( a + " and " +  b  + " are equal");
-    }
-    sc.close();
-    }
-}
->>>>>>> 7f015239870e0555467dcecdd9ed6af61399192e
