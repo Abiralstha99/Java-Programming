01dfb9c2cee0321598ae1e9f74e6f98b6d1eea64
Abiral Stha <abiralstha333@gmail.com>
2024-03-22 23:21:17 +0545

    Java Day 7

---
+++ Function.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Function {
+    /*
+     * First function - To print name
+     * public static void printName(String name) {
+     * System.out.println(name);
+     * return;
+     * }
+     * 
+     * 
+     * public static int multiply(int a, int b) {
+     * int mult = a * b;
+     * return mult;
+     * }
+     */
+
+    // public static int multiply(int a, int b) {
+    // int mult = a * b;
+    // return mult;
+    // }
+
+    // public static void main(String[] args) {
+    // Scanner sc = new Scanner(System.in);
+    // int a = sc.nextInt();
+    // int b = sc.nextInt();
+    // int sum = multiply(a, b);
+    // System.out.println(sum);
+    // sc.close();
+    // }
+
+    // Function for a factorial
+
+    public static void fact(int a) {
+        int n = 1;
+        for (int i = 1; i <= a; i++) {
+            n = n * i;
+        }
+        System.out.println("The factorial is " + n);
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enter the number");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        fact(num);
+    }
+
+}
