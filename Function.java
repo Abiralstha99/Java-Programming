6b8eafe5a4924b30d9a78b0c7afdcdd545367285
Abiral Stha <abiralstha333@gmail.com>
2024-03-24 21:51:51 +0545

    Day 8

--- Function.java
+++ Function.java
@@ -50,43 +50,26 @@ public class Function {
 
     // Program to calculate prime or composite
 
-    /*
-     * public static void calculate(int n) {
-     * int j = 0;
-     * for (int i = 1; i < n; i++) {
-     * if (n % i == 0) {
-     * j++;
-     * }
-     * }
-     * if (j == 1) {
-     * System.out.print("Prime");
-     * } else {
-     * System.err.print("Composite");
-     * }
-     * }
-     * 
-     * public static void main(String[] args) {
-     * System.out.print("Enter a number");
-     * Scanner sc = new Scanner(System.in);
-     * int n = sc.nextInt();
-     * calculate(n);
-     * }
-     */
-
-    // WAP to print multiplication table of a number
-
-    public static void mult(int num) {
-        for (int i = 1; i <= 10; i++) {
-            int n = num * i;
-            System.out.println(num + " * " + i + " = " + n);
+    public static void calculate(int n) {
+        int j = 0;
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                j++;
+            }
+        }
+        if (j == 1) {
+            System.out.print("Prime");
+        } else {
+            System.err.print("Composite");
         }
     }
 
     public static void main(String[] args) {
-        System.out.println("Enter the number");
+        System.out.print("Enter a number");
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        mult(num);
+        int n = sc.nextInt();
+        calculate(n);
+        sc.close();
     }
 
 }
